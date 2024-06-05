b4a317223ac9b2e3846d29eca3cf2686e739e4d2
Lucas Katacha <lucasgabrieljaci19@gmail.com>
2024-06-04 22:18:43 -0300

    'tirei-botao-ajuda-2'

--- apps/builder/src/features/editor/components/TypebotHeader.tsx
+++ apps/builder/src/features/editor/components/TypebotHeader.tsx
@@ -12,7 +12,7 @@ import {
   chakra,
 } from '@chakra-ui/react'
 import {
-  BuoyIcon,
+  //BuoyIcon,
   ChevronLeftIcon,
   CopyIcon,
   PlayIcon,
@@ -46,11 +46,11 @@ export const TypebotHeader = () => {
   const { isOpen, onOpen } = useDisclosure()
   const headerBgColor = useColorModeValue('white', 'gray.900')
 
-  const handleHelpClick = () => {
+  /*const handleHelpClick = () => {
     isCloudProdInstance() && workspace?.plan && workspace.plan !== Plan.FREE
       ? onOpen()
       : window.open('https://docs.typebot.io/guides/how-to-get-help', '_blank')
-  }
+  }*/
 
   if (currentUserMode === 'guest') return <GuestTypebotHeader />
   return (
@@ -66,7 +66,7 @@ export const TypebotHeader = () => {
       flexShrink={0}
     >
       {isOpen && <SupportBubble autoShowDelay={0} />}
-      <LeftElements pos="absolute" left="1rem" onHelpClick={handleHelpClick} />
+      <LeftElements pos="absolute" left="1rem" {/*onHelpClick={handleHelpClick}*/} />
       <TypebotNav
         display={{ base: 'none', xl: 'flex' }}
         pos={{ base: 'absolute' }}
