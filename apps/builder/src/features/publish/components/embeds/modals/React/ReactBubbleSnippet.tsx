139c972735ad8f456f267f084e351531744bde3d
Lucas Katacha <lucasgabrieljaci19@gmail.com>
2024-06-28 10:12:32 -0300

    'bubble-correct'

--- apps/builder/src/features/publish/components/embeds/modals/React/ReactBubbleSnippet.tsx
+++ apps/builder/src/features/publish/components/embeds/modals/React/ReactBubbleSnippet.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { CodeEditor } from '@/components/inputs/CodeEditor'
 import { useTypebot } from '@/features/editor/providers/TypebotProvider'
 import { BubbleProps } from '@typebot.io/nextjs'
